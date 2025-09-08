874e371f025597dc79d6faa6060b450e774bceb0
jcchincheong-bb <jc.chincheong@gmail.com>
2025-09-08 02:09:35 +0200

    Ultrasonic Sensor Logic Setup
    - logic for US sensor to be readable
    - very simple emergency stop for when sensor detects obstacle within 10 cm

--- RobCar_Arduino/RobCar_MotorControl/RobCar_MotorControl.ino
+++ RobCar_Arduino/RobCar_MotorControl/RobCar_MotorControl.ino
@@ -1,46 +1,85 @@
 // ***************** PINS **********************
+// Ultrasonic Sensor
+#define Trigger 12
+#define Echo 13
+
 // Front Right Motor (FR)
-int motorFRpin1 = 5;
-int motorFRpin2 = 4;
+int FR_f = 7;  // forward (f)
+int FR_r = 6;  // reverse (r)
 
 // Front Left Motor (FL)
-int motorFLpin1 = 3;
-int motorFLpin2 = 2;
+int FL_f = 9;
+int FL_r = 8;
+
+// Rear Right Motor (RR)
+int RR_f = 2;
+int RR_r = 3;
+
+// Rear Left Motor (RL)
+int RL_f = 4;
+int RL_r = 5;
+
 // ********************************************
 
 void setup() {
   // Set all motor pins to output
-  pinMode(motorFRpin1,OUTPUT);
-  pinMode(motorFRpin2,OUTPUT);
-  pinMode(motorFLpin1,OUTPUT);
-  pinMode(motorFLpin2,OUTPUT);
-
+  pinMode(FR_f, OUTPUT);
+  pinMode(FR_r, OUTPUT);
+  pinMode(FL_f, OUTPUT);
+  pinMode(FL_r, OUTPUT);
+  pinMode(RR_f, OUTPUT);
+  pinMode(RR_r, OUTPUT);
+  pinMode(RL_f, OUTPUT);
+  pinMode(RL_r, OUTPUT);
+  // Set Echo and Trigger for US Sensor
+  pinMode(Echo, INPUT);
+  pinMode(Trigger, OUTPUT);
+  // Serial Initilisation
+  Serial.begin(9600);
 }
 
 void loop() {
-  // Test loop to switch diretion after each second [Ryan Chan]
-  // Forward for 2 seconds
-  digitalWrite(motorFRpin1, HIGH);
-  digitalWrite(motorFRpin2, LOW);
+  // Set Trigger LOW
+  digitalWrite(Trigger, LOW);
+  delayMicroseconds(2);
+  
+  // Emit high frequency 40kHz sound pulse (i.e. pull the Trigger)
+  digitalWrite(Trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Trigger, LOW);
 
-  digitalWrite(motorFLpin1, HIGH);
-  digitalWrite(motorFLpin2, LOW);
-  delay(2000);
+  // Detect a pulse
+  int distance = pulseIn(Echo, HIGH);
 
-  // Stop for 1 sec
-  digitalWrite(motorFRpin1, LOW);
-  digitalWrite(motorFRpin2, LOW);
-
-  digitalWrite(motorFLpin1, LOW);
-  digitalWrite(motorFLpin2, LOW);
-  delay(1000);
-
-  // Reverse for 2 secs
-  digitalWrite(motorFRpin1, LOW);
-  digitalWrite(motorFRpin2, HIGH);
-
-  digitalWrite(motorFLpin1, LOW);
-  digitalWrite(motorFLpin2, HIGH);
-  delay(2000);
-
+  // Speed of sound is: 343 m/s -> 343 x 10^2 cm/s -> 343 x 10^2 x 10^-6 cm/us -> 29.15451895 us/cm
+  distance = distance / 29 / 2;  // in cm
+  // we divide by 2 to acount for the round trip
+  
+  // Driving Logic
+  if (distance <= 10){
+    // Emergency stop
+    digitalWrite(FR_f, LOW);
+    digitalWrite(FR_r, LOW);
+    digitalWrite(FL_f, LOW);
+    digitalWrite(FL_r, LOW);
+    digitalWrite(RR_f, LOW);
+    digitalWrite(RR_r, LOW);
+    digitalWrite(RL_f, LOW);
+    digitalWrite(RL_r, LOW);
+  } else {
+    // Forward Drive
+    digitalWrite(FR_f, HIGH);
+    digitalWrite(FR_r, LOW);
+    digitalWrite(FL_f, HIGH);
+    digitalWrite(FL_r, LOW);
+    digitalWrite(RR_f, HIGH);
+    digitalWrite(RR_r, LOW);
+    digitalWrite(RL_f, HIGH);
+    digitalWrite(RL_r, LOW);
+  }
+  // Print the distance in inches
+  Serial.println(distance);
+  
+  // Pause for 100 milliseconds
+  delay(200);
 }
